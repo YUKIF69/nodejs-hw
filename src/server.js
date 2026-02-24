bc9c6593ddb03ff7d157845b970ea1c15646ca15
YUKIF69 <159157261+YUKIF69@users.noreply.github.com>
2026-02-24 19:33:07 +0200

    first

---
+++ src/server.js
@@ -0,0 +1,60 @@
+import 'dotenv/config';
+import cors from 'cors';
+import pino from 'pino-http';
+import express from 'express';
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  req.log.info('Отримання усіх нотаток');
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  req.log.info(`Отримання нотатки з ID: ${noteId}`);
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((err, req, res, next) => {
+  if (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
